e3af33a3dedc2ba4b17536cf2ff8a126d9720216
Николай Бухалов <nbuhalov@team.amocrm.com>
2024-01-15 19:14:19 +0300

    added struct tags for db to models

--- internal/application/models/tool.go
+++ internal/application/models/tool.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Tool struct {
-	Name           string    `json:"name"`
-	Description    string    `json:"description"`
-	Equipment      string    `json:"equipment"`
-	WearRate       int       `json:"wear_rate"`
-	Supplier       string    `json:"supplier"`
-	DateOfPurchase time.Time `json:"date_of_purchase"`
-	Count          int       `json:"count"`
+	Name           string    `json:"name" db:"name"`
+	Description    string    `json:"description" db:"description"`
+	Equipment      string    `json:"equipment" db:"equipment"`
+	WearRate       int       `json:"wear_rate" db:"wear_rate"`
+	Supplier       string    `json:"supplier" db:"supplier"`
+	DateOfPurchase time.Time `json:"date_of_purchase" db:"date_of_purchase"`
+	Count          int       `json:"count" db:"count"`
 }
--- internal/application/models/user.go
+++ internal/application/models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	Login    string `json:"login"`
-	Password string `json:"password"`
-	Role     string `json:"role,omitempty"`
-	FullName string `json:"full_name,omitempty"`
-	Photo    string `json:"photo,omitempty"`
+	Login    string `json:"login" db:"login"`
+	Password string `json:"password" db:"password"`
+	Role     string `json:"role,omitempty" db:"role"`
+	FullName string `json:"full_name,omitempty" db:"full_name"`
+	Photo    string `json:"photo,omitempty" db:"photo"`
 }
